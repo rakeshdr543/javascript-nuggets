80854ba99b05c6db571418c517704a989c660cd4
Rakesh DR <rakeshdr543@gmail.com>
2021-06-13 11:19:27 +0530

    Init js commit

---
+++ callback-hell/app.js
@@ -0,0 +1,10 @@
+//  Javascript Nuggets - Callback Hell
+// after 1s first red;
+// after 3s second blue; 4s
+// after 2s third green; 6s
+// IN SEQUENCE !!!!
+
+const first = document.querySelector('.first')
+const second = document.querySelector('.second')
+const third = document.querySelector('.third')
+const btn = document.querySelector('.btn')
